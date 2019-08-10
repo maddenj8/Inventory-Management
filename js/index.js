319bb16d0176de85b691c7a9d7ad3b4484e1c382
Jason Madden <jason.madden8@mail.dcu.ie>
2019-08-10 11:07:33 +0100

    trying to get file drop working

--- js/index.js
+++ js/index.js
@@ -26,18 +26,11 @@ app.controller('viewController', function($scope) {
     })
 })
 
-app.controller('boxController', function($scope) {
-
-})
-
-app.controller('itemController', ['$routeParams', function($routeParams) {
-    this.id = $routeParams.id
-}])
-
 app.controller('topbar', function($scope) {
     $scope.items = ["this", "that", "other"]
 }) 
 
+// --------- FUNCTIONS FOR BOXES --------- //
 app.controller('boxes', function($scope) {
     $scope.boxes = [
         {code: "Y05",
@@ -56,8 +49,87 @@ app.controller('boxes', function($scope) {
     ]
 })
 
-app.controller('testCtrl', function($scope) {
-    $scope.name = "Jason Madden"
+app.controller('boxController', function($scope) {
+
+})
+
+// -------- FUNCTIONS FOR ITEMS ----------- //
+app.controller('itemController', ['$routeParams', function($routeParams) {
+    this.id = $routeParams.id
+}])
+
+app.directive('img-upload', function($http, $compile) {
+    return {
+        restrict: 'AE', 
+        scope: {
+            url:'@', 
+            method:'@'
+        },
+        link: function(scope, elem, attrs) {
+            var formData = new FormData()
+            scope.previewData = [] // list of images uploaded
+
+            function previewData (file) {
+                var reader = new FileReader();
+                var obj = new FormData().append('file',file);			
+                reader.onload=function(data){
+                    var src = data.target.result;
+                    var size = ((file.size/(1024*1024)) > 1)? (file.size/(1024*1024)) + ' mB' : (file.size/		1024)+' kB';
+                    scope.$apply(function(){
+                        scope.previewData.push({'name':file.name,'size':size,'type':file.type,
+                                                'src':src,'data':obj});
+                    });								
+                    console.log(scope.previewData);
+                }
+                reader.readAsDataURL(file);
+            }
+            function uploadFile(e,type){
+                e.preventDefault();			
+                var files = "";
+                if(type == "formControl"){
+                    files = e.target.files;
+                } else if(type === "drop"){
+                    files = e.originalEvent.dataTransfer.files;
+                }			
+                for(var i=0;i<files.length;i++){
+                    var file = files[i];
+                    if(file.type.indexOf("image") !== -1){
+                        previewFile(file);								
+                    } else {
+                        alert(file.name + " is not supported");
+                    }
+                }
+            }
+            elem.find('.fileUpload').bind('change',function(e){
+                uploadFile(e,'formControl');
+            });
+
+            elem.find('.dropzone').bind("click",function(e){
+                $compile(elem.find('.fileUpload'))(scope).trigger('click');
+                // console.log('element was clicked')
+            });
+
+            elem.find('.dropzone').bind("dragover",function(e){
+                e.preventDefault();
+            });
+
+            elem.find('.dropzone').bind("drop",function(e){
+                uploadFile(e,'drop');																		
+            });
+            scope.upload=function(obj){
+                $http({method:scope.method,url:scope.url,data: obj.data,
+                    headers: {'Content-Type': undefined},transformRequest: angular.identity
+                }).success(function(data){
+
+                });
+            }
+
+            scope.remove=function(data){
+                var index= scope.previewData.indexOf(data);
+                scope.previewData.splice(index,1);
+            }	
+        }
+    }
 })
 
 function fill_todays_date() {
